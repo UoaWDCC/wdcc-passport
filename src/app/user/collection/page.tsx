7a43609d9b9783dc88ed915077f1ff8e723e3083
shabebe11 <salk147@aucklanduni.ac.nz>
2026-05-09 14:55:29 +1200

    feature: page changes

--- src/app/user/collection/page.tsx
+++ src/app/user/collection/page.tsx
@@ -1,6 +1,5 @@
 import CollectionMainContent from "@/components/user/collection/CollectionMainContent";
 
-
 export default function UserPackCollectionPage() {
   return (
     <section className="h-full">
