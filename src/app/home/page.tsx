957248075aeec0326655a87d350350a283e72fbc
Koutaro Yumiba <89110272+koutaroyumiba@users.noreply.github.com>
2026-05-30 15:21:49 +1200

    Kot/tanstack query (#3)
    * feat: tanstack query
    
    * chore: put db under lib
    
    * chore: changed to wildcard
    
    * feat: logged in feedback

---
+++ src/app/home/page.tsx
@@ -0,0 +1,3 @@
+export default function Home() {
+	return <div>logged in successfully</div>
+}
--- src/db/schema/index.ts
+++
@@ -1,4 +0,0 @@
-export { user } from "./user.schema";
-export { account } from "./account.schema";
-export { session } from "./session.schema";
-export { verification } from "./verification.schema";
