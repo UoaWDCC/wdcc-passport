18ee51544e4b7d0c379cef00e81fe3e16b2aae04
Kot <89110272+koutaroyumiba@users.noreply.github.com>
2026-05-30 13:41:30 +1200

    feat: auth using better auth

--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,1 +1,5 @@
-// blank for now
+// auth
+import { user } from "./user.schema"
+import { account } from "./account.schema"
+import { session } from "./session.schema"
+import { verification } from "./verification.schema"
