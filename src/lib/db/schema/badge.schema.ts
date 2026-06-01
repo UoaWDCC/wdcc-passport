578bbe4a6703ea92bb259be95f3a0ea957660482
Shuaib Al Khudairi <alkhudairi.sa@gmail.com>
2026-06-01 20:04:55 +1200

    feat: add index for eventId in badges schema

--- src/lib/db/schema/badge.schema.ts
+++ src/lib/db/schema/badge.schema.ts
@@ -1,13 +1,17 @@
-import { pgEnum, pgTable, text } from "drizzle-orm/pg-core";
+import { index, pgEnum, pgTable, text } from "drizzle-orm/pg-core";
 
 import { event } from "./event.schema";
 
 export const badgeType = pgEnum("badge_type", ["event", "special"]);
 
-export const badge = pgTable("badge", {
-  id: text("id").primaryKey(),
-  name: text("name").notNull(),
-  path: text("path").notNull(),
-  type: badgeType("type").notNull(),
-  eventId: text("event_id").references(() => event.id),
-});
+export const badge = pgTable(
+  "badge",
+  {
+    id: text("id").primaryKey(),
+    name: text("name").notNull(),
+    path: text("path").notNull(),
+    type: badgeType("type").notNull(),
+    eventId: text("event_id").references(() => event.id),
+  },
+  (table) => [index("badge_eventId_idx").on(table.eventId)],
+);
