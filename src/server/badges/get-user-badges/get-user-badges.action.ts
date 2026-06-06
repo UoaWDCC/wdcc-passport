b0fba1cd1174c4a576cb4715083aa20e121926c0
Mkua509 <maternuskuang@gmail.com>
2026-06-07 02:55:54 +1200

    chore: prettier again

--- src/server/badges/get-user-badges/get-user-badges.action.ts
+++ src/server/badges/get-user-badges/get-user-badges.action.ts
@@ -3,9 +3,7 @@
 import { getUserBadges } from "@/server/badges/get-user-badges/get-user-badges.service";
 import { requireUser } from "@/lib/access";
 
-
 export async function getUserBadgesAction() {
   const session = await requireUser();
   return await getUserBadges(session.user.id);
 }
-
