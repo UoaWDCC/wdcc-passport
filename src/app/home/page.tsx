aeb06f2cf13711988845c0e962cdce6450b22898
Shuaib Al Khudairi <123057239+shabebe11@users.noreply.github.com>
2026-06-07 14:00:06 +1200

    feature/get-badges-fe (#12)
    Co-authored-by: Shuaib Al Khudairi <alkhudairi.sa@gmail.com>

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,12 +1,10 @@
-import { getUserBadgesAction } from "@/server/badges/get-user-badges/get-user-badges.action";
+import { BadgesSection } from "@/components/home/BadgesSection";
 
 export default async function Home() {
-  const badges = await getUserBadgesAction();
-
   return (
     <main>
-      <h1>Test JSON</h1>
-      <pre>{JSON.stringify(badges, null, 2)}</pre>
+      <h1>Home</h1>
+      <BadgesSection />
     </main>
   );
 }
