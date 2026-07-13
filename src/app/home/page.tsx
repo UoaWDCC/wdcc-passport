c1303245e4f51c8418463fb19a69a02f9261fcae
Maternus Kuang <maternuskuang@gmail.com>
2026-07-14 01:55:13 +1200

    feat: created server action for create badge (#16)
    * feat: created server action for create badge
    
    * chore: typo fixed for create-badge.service
    
    * chore: prettier ran
    
    * chore: rename test badge button to CreateBadgeButton

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,10 +1,11 @@
 import { BadgesSection } from "@/components/home/BadgesSection";
-
+import { CreateBadgeButton } from "@/components/CreateBadgeButton";
 export default async function Home() {
   return (
     <main>
       <h1>Home</h1>
       <BadgesSection />
+      <CreateBadgeButton />
     </main>
   );
 }
