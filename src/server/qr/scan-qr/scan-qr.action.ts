bcc6f8dd46f0ef686eb46da86b34b546e3f2cd03
shabebe11 <salk147@aucklanduni.ac.nz>
2026-08-19 16:07:29 +1200

    feat: make the service and action

--- src/server/qr/scan-qr/scan-qr.action.ts
+++ src/server/qr/scan-qr/scan-qr.action.ts
@@ -1,4 +1,4 @@
-"user server"
+"use server";
 
 import { requireUser } from "@/lib/access";
 import { scanQr } from "./scan-qr.service";
@@ -6,8 +6,5 @@ import { scanQr } from "./scan-qr.service";
 export async function scanQrAction(code: string) {
   const session = await requireUser();
 
-  if(code.length > 6){
-    code = code.slice(0, 6);
-  }
   return scanQr(code, session.user.id);
 }
