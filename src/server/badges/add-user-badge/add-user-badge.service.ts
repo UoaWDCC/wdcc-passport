d13b4b69e4fb63f3d959f5fadea75b1a91ea866c
shabebe11 <salk147@aucklanduni.ac.nz>
2026-08-20 17:00:29 +1200

    chore: formate and stuff

--- src/server/badges/add-user-badge/add-user-badge.service.ts
+++ src/server/badges/add-user-badge/add-user-badge.service.ts
@@ -2,12 +2,11 @@ import { db } from "@/lib/db/client";
 import { userBadge } from "@/lib/db/schema";
 
 export async function addUserBadge(userId: string, badgeId: string) {
-
-const [awarded] = await db
+  const [awarded] = await db
     .insert(userBadge)
     .values({ userId, badgeId })
     .onConflictDoNothing()
     .returning();
 
-    return { badgeId, alreadyAwarded: !awarded};
+  return { badgeId, alreadyAwarded: !awarded };
 }
