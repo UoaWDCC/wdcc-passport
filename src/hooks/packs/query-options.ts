69450f9c442c4107907258ae7800bb0a8ddc9606
shabebe11 <salk147@aucklanduni.ac.nz>
2026-09-16 16:17:04 +1200

    feat: sort cards when pack open and also get pack quantity

--- src/hooks/packs/query-options.ts
+++ src/hooks/packs/query-options.ts
@@ -1,4 +1,9 @@
-import { openPackAction } from "@/server/packs/action";
+import { getUserPackCountAction, openPackAction } from "@/server/packs/action";
+
+export const getUserPackCountQuery = () => ({
+  queryKey: ["get-user-pack-count"],
+  queryFn: getUserPackCountAction,
+});
 
 export const openPackMutation = (options?: { onSuccess?: () => void }) => ({
   mutationFn: openPackAction,
--- src/server/packs/action.ts
+++ src/server/packs/action.ts
@@ -3,6 +3,7 @@
 import { requireUser } from "@/lib/access";
 import { openPack } from "./mutation";
 import { generateCards } from "../cards/queries";
+import { getUserPackCount } from "./queries";
 
 export async function openPackAction() {
   const session = await requireUser();
@@ -14,4 +15,10 @@ export async function openPackAction() {
   }
 
   return cards;
+}
+
+export async function getUserPackCountAction() {
+  const session = await requireUser();
+
+  return getUserPackCount(session.user.id);
 }
